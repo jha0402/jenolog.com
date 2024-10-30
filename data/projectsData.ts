4a8ea20807e3bb27c9439a92a0e2f5db6c9d997f
jha0402 <67306416+jha0402@users.noreply.github.com>
2024-10-30 15:12:11 +0900

    fix: project cards image versioning

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -22,35 +22,35 @@ const projectsData: ProjectsData = {
       title: 'Joonggonara Web Service',
       description: `I was responsible for project creation, refactoring, removing legacy code, initial setup, building the CI/CD pipeline, SEO optimization, performance optimization, and integrating the app with universal links / deep links.
       Tech stack used: Typescript, React, Next.js, Recoil, React Query, Tailwind CSS, AWS`,
-      imgSrc: 'blog/joonggonara_wicoyq.png',
+      imgSrc: 'v1730207063/blog/joonggonara_wicoyq.png',
       href: 'https://web.joongna.com',
     },
     {
       title: 'Joonggonara App Webview',
       description: `I was responsible for maintaining, refactoring, removing legacy code, optimizing performance, and integrating the app with universal links / deep links for payment/event/FAQ/user pages.
       Tech stack used: Typescript, Recoil, React Query, Emotion, AWS`,
-      imgSrc: 'blog/joonggonara_wicoyq.png',
+      imgSrc: 'v1730207063/blog/joonggonara_wicoyq.png',
       href: 'https://apps.apple.com/kr/app/%EC%A4%91%EA%B3%A0%EB%82%98%EB%9D%BC-%EA%B5%AD%EB%82%B4-%EC%B5%9C%EB%8C%80-%EC%A4%91%EA%B3%A0%EB%A7%88%EC%BC%93/id896515652',
     },
     {
       title: 'Vanity Table',
       description: `I was responsible for development maintenance, SEO optimization, performance optimization, and developing a Shopify app for order/inventory management.
       Tech stack used: Javascript, Node.js, Jquery, Shopify, Liquid`,
-      imgSrc: 'blog/vntable_uj2oua.png',
+      imgSrc: 'v1730207845/blog/vntable_uj2oua.png',
       href: 'https://vanity-table.com',
     },
     {
       title: 'Fingersuit US/JP',
       description: `I was responsible for development maintenance, SEO optimization, and performance optimization.
       Tech stack used: Javascript, Jquery, Shopify, Liquid`,
-      imgSrc: 'blog/fingersuitOgImg_hfy9yu.png',
+      imgSrc: 'v1730207876/blog/fingersuitOgImg_hfy9yu.png',
       href: 'https://fingersuit.com',
     },
     {
       title: 'Andar Global',
       description: `I was responsible for development maintenance, SEO optimization, and performance optimization.
       Tech stack used: Javascript, Jquery, Shopify, Liquid`,
-      imgSrc: 'blog/andar_hzdgxs.png',
+      imgSrc: 'v1730207887/blog/andar_hzdgxs.png',
       href: 'https://andar-global.com',
     },
   ],
@@ -67,35 +67,35 @@ const projectsData: ProjectsData = {
       title: '중고나라 웹 서비스',
       description: `프로젝트 생성, 리팩토링, 레거시 제거, 초기 설정, CI/CD 파이프라인 구축, SEO 최적화, 성능 최적화, 앱 universal link / deep link 연동 등 담당했습니다.
       사용한 기술 스택: Typescript, React, Next.js, Recoil, React Query, Tailwind CSS, AWS`,
-      imgSrc: 'blog/joonggonara_wicoyq.png',
+      imgSrc: 'v1730207063/blog/joonggonara_wicoyq.png',
       href: 'https://web.joongna.com',
     },
     {
       title: '중고나라 앱 웹뷰',
       description: `결제/이벤트/FAQ/유저 페이지 유지보수, 리팩토링, 레거시 제거, 성능 최적화, 앱 universal link / deep link 연동 등 담당했습니다.
       사용한 기술 스택: Typescript, Recoil, React Query, Emotion, AWS`,
-      imgSrc: 'blog/joonggonara_wicoyq.png',
+      imgSrc: 'v1730207063/blog/joonggonara_wicoyq.png',
       href: 'https://apps.apple.com/kr/app/%EC%A4%91%EA%B3%A0%EB%82%98%EB%9D%BC-%EA%B5%AD%EB%82%B4-%EC%B5%9C%EB%8C%80-%EC%A4%91%EA%B3%A0%EB%A7%88%EC%BC%93/id896515652',
     },
     {
       title: 'Vanity Table',
       description: `개발 유지보수, SEO 최적화, 성능 최적화, 주문/재고 관리 Shopify 앱 개발을 담당했습니다. 
       사용한 기술 스택: Javascript, Node.js, Jquery, Shopify, Liquid`,
-      imgSrc: 'blog/vntable_uj2oua.png',
+      imgSrc: 'v1730207845/blog/vntable_uj2oua.png',
       href: 'https://vanity-table.com',
     },
     {
       title: 'Fingersuit US/JP',
       description: `개발 유지보수, SEO 최적화, 성능 최적화를 담당했습니다. 
       사용한 기술 스택: Javascript, Jquery, Shopify, Liquid`,
-      imgSrc: 'blog/fingersuitOgImg_hfy9yu.png',
+      imgSrc: 'v1730207876/blog/fingersuitOgImg_hfy9yu.png',
       href: 'https://fingersuit.com',
     },
     {
       title: 'Andar Global',
       description: `개발 유지보수, SEO 최적화, 성능 최적화를 담당했습니다. 
       사용한 기술 스택: Javascript, Jquery, Shopify, Liquid`,
-      imgSrc: 'blog/andar_hzdgxs.png',
+      imgSrc: 'v1730207887/blog/andar_hzdgxs.png',
       href: 'https://andar-global.com',
     },
   ],
